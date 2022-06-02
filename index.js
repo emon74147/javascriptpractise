9a81b14ea9fc8f8624e8826dcae718e0cd9f57b8
emon74147 <dev.emon74@gmail.com>
2022-06-02 21:18:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// function myFunction(name, time){
+//     console.log( name + " sleeping from " + time);
+// }
+// myFunction("rahat", "11");
+// myFunction("emon", "10");
+// myFunction("pagla", "9");
+
+
+
+
+// function myFunction(a, b) {
+//     var result = a*b;
+//     return result;
+// }
+
+// console.log(myFunction(6,6));
+
+   
+
+function myFunction(a, b) {
+  return a * b;       
+}
+
+console.log(myFunction(9, 9));
