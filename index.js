7dd0695f0509aaf7e7381d0a4f6eda6e79832464
emon74147 <dev.emon74@gmail.com>
2022-06-03 17:54:04 +0600

    added new code

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// function myFunction(name, time){
-//     console.log( name + " sleeping from " + time);
-// }
-// myFunction("rahat", "11");
-// myFunction("emon", "10");
-// myFunction("pagla", "9");
+function myFunction(name, time){
+    console.log( name + " sleeping from " + time);
+}
+myFunction("rahat", "11");
+myFunction("emon", "10");
+myFunction("pagla", "9");
 
 
 
 
-// function myFunction(a, b) {
-//     var result = a*b;
-//     return result;
-// }
+function myFunction(a, b) {
+    var result = a*b;
+    return result;
+}
 
-// console.log(myFunction(6,6));
+console.log(myFunction(6,6));
 
    
 
@@ -22,3 +22,30 @@ function myFunction(a, b) {
 }
 
 console.log(myFunction(9, 9));
+
+let Spiderman = {
+  firstName : "Emon",
+  lastName  : "Ahmad",
+  age       : 21,
+
+  fullName: function (){
+    return this.firstName + " " + this.lastName;
+    
+  }
+}
+Spiderman.fullName();
+console.log(Spiderman.age);
+
+
+const avengers = ["ironman","hulk","thor","captain america"];
+
+console.log(avengers.indexOf(3))
+
+const ages = [3, 10, 18, 20];
+
+ages.findIndex(checkAge);
+
+function checkAge(age) {
+  return age > 18;
+};
+console.log(checkAge(20))
